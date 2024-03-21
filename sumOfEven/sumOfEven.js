e2500f1cb526d749de0a6ee352eab17db11e0dac
Ano Jumisa <anojumisa@gmx.de>
2024-03-21 10:39:28 +0800

    Complete challenge sumOfEven and sumofSquares

--- sumOfEven/sumOfEven.js
+++ sumOfEven/sumOfEven.js
@@ -1,5 +1,14 @@
 function sumOfEvens(numbers) {
-  // Your code here
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (isEven(numbers[i])) {
+            sum += numbers[i];
+        }
+    }
+    return sum;
+}
+function isEven(number) {
+    return number % 2 === 0;
 }
 
 module.exports = sumOfEvens;
--- sumOfSquares/sumSquares.js
+++ sumOfSquares/sumSquares.js
@@ -1,5 +1,11 @@
 function sumOfSquares(num1, num2) {
-  // Your code here
+    // Your code here
+    let sum = 0;
+
+    sum += Math.pow(num1, 2);
+    sum += Math.pow(num2, 2);
+
+    return sum;
 }
 
 module.exports = sumOfSquares;
