9e7aa9407ffde9d39ca2d1b487b2e796fe0e257f
Ano Jumisa <anojumisa@gmx.de>
2024-03-22 11:57:15 +0800

    Submit challenge for sumClassifier

--- sumClassifier/sumClassifier.js
+++ sumClassifier/sumClassifier.js
@@ -1,5 +1,17 @@
 function sumClassifier(numbers) {
-  // Your code here
+    // Your code here
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+
+    if (sum > 10) {
+        return "Large Sum";
+    } else if (sum === 10) {
+        return "Equal Sum";
+    } else {
+        return "Small Sum";
+    }
 }
 
 module.exports = sumClassifier;
